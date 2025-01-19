a897fd093bacb09f3fefd7fcabe335fb926e7593
Lev Karavanov <153912980+levkaravanov@users.noreply.github.com>
2025-01-19 19:29:41 +0200

    feat: complete task 3 with rectangle perimeter and area calculation

---
+++ 2_3_Suorakulmio.py
@@ -0,0 +1,6 @@
+sideA = float(input("Anna suorakulmion kanta: "))
+sideB = float(input("Anna suorakulmion korkeus: "))
+rectanglePerimeter = 2 * (sideA + sideB)
+rectangleArea = sideA * sideB
+print(f"Suorakulmion piiri on {rectanglePerimeter}")
+print(f"Suorakulmion pinta-ala on {rectangleArea}")
