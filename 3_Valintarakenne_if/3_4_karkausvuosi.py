50aded92114173e68a098be6ee0a04d3862c2865
Lev Karavanov <153912980+levkaravanov@users.noreply.github.com>
2025-01-23 11:05:10 +0200

    Yksinkertaistettu karkausvuoden tarkistava ohjelma selkeämmäksi ja tehokkaammaksi.

--- 3_Valintarakenne_if/3_4_karkausvuosi.py
+++ 3_Valintarakenne_if/3_4_karkausvuosi.py
@@ -1,14 +1,7 @@
 vuosi = int(input("Anna vuosi muodossa YYYY: "))
 if vuosi > 0:
-    if vuosi % 400 == 0:
+    if vuosi % 4 == 0 and (vuosi % 100 != 0 or vuosi % 400 == 0):
         print(f"{vuosi} on karkausvuosi")
-    elif vuosi % 4 == 0:
-        if vuosi % 100 == 0 and vuosi % 400 == 0:
-            print(f"{vuosi} on karkausvuosi")
-        elif vuosi % 100 == 0 and vuosi % 400 != 0:
-            print(f"{vuosi} ei ole karkausvuosi")
-        else:
-            print(f"{vuosi} on karkausvuosi")
     else:
         print(f"{vuosi} ei ole karkausvuosi")
 else:
