baf96eea29796ad354b0a595b2a31bbda2d0bd2c
Lev Karavanov <153912980+levkaravanov@users.noreply.github.com>
2025-01-23 10:06:49 +0200

    feat: lisätty ohjelma hemoglobiiniarvon tarkistamiseen sukupuolen perusteella

---
+++ 3_3_hemoglobiiniarvo.py
@@ -0,0 +1,24 @@
+sukupuoli = input("Syötä sukupuoli (nainen/mies):")
+if sukupuoli == "nainen" or sukupuoli == "mies":
+    hemoglobiiniarvo = float(input("Syötä hemoglobiiniarvo (g/l):"))
+
+    alhainen = "Hemoglobiiniarvo on alhainen."
+    korkea = "Hemoglobiiniarvo on korkea."
+    normaali = "Hemoglobiiniarvo on normaali."
+
+    if sukupuoli == "nainen":
+        if hemoglobiiniarvo < 117:
+            print(alhainen)
+        elif hemoglobiiniarvo > 175:
+            print(korkea)
+        else:
+            print(normaali)
+    else:
+        if hemoglobiiniarvo < 134:
+            print(alhainen)
+        elif hemoglobiiniarvo > 195:
+            print(korkea)
+        else:
+            print(normaali)
+else:
+    print("Virheellinen sukupuoli. Anna sukupuoli muodossa ‘nainen’ tai ‘mies’.")
