09964817b2a4db59576aa0f82b087ca4121a2b2f
Lev Karavanov <153912980+levkaravanov@users.noreply.github.com>
2025-01-31 10:06:23 +0200

    feat: Toteutettu ohjelma, joka lajittelee annetut luvut ja tulostaa viisi suurinta lukua järjestyksessä

---
+++ 5_Listarakenne_ja_läpikäyvä_toistorakenne/5_2.py
@@ -0,0 +1,15 @@
+numbers = []
+
+while True:
+    luku = input("Anna luku: ")
+    if luku == "":
+        break
+    numbers.append(int(luku))
+
+numbers.sort(reverse=True)
+
+if numbers:
+    for x in numbers[:5]:
+        print(x)
+else:
+    print("Et antanut yhtään lukua.")
